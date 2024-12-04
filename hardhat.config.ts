7394fbab0bc3d1e99c9883e9f1b05d1bf9437013
Aladeenb <alabtk@gmail.com>
2024-12-04 11:04:10 +0100

    Added test coverage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import '@openzeppelin/hardhat-upgrades';
 import "@nomicfoundation/hardhat-toolbox";
+import "solidity-coverage";
 
 const config: HardhatUserConfig = {
-  // solidity: "0.8.22",
   solidity: {
     compilers: [
       {
