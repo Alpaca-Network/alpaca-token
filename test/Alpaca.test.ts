344651350b136ae168133157c82a96a788dfa7d1
john-Alpaca <john@llamanetwork.ai>
2024-12-10 08:22:59 -0800

    Added unit test for contract owner being 0x0

--- test/Alpaca.test.ts
+++ test/Alpaca.test.ts
@@ -107,5 +107,29 @@ describe("Alpaca local test", async () => {
             const isZero = await PacaToken.hasRole(UPGRADER_ROLE, zeroAddress);
             expect(isZero).to.equal(true)
         });
+        it("Working all the functionalities for Owner being a dead wallet", async function () {
+
+            const DEFAULT_ADMIN_ROLE = await PacaToken.DEFAULT_ADMIN_ROLE();
+            await PacaToken.connect(owner).grantRole(DEFAULT_ADMIN_ROLE, zeroAddress);
+
+            await PacaToken.connect(taxAdmin).updateFees(1000, 500); // 10%/5% buy/sell tax fee
+            await PacaToken.connect(taxAdmin).updateTreasuryWallet(user2.address);
+            await PacaToken.connect(taxAdmin).addDex(user1.address, true);
+            
+            // Simulate a transfer (buy scenario)
+            await expect(PacaToken.connect(owner).transfer(user1.address, 20000))
+                .to.emit(PacaToken, "FeeCharged")
+                .withArgs(PacaToken.treasury(), 1000);
+            expect(await PacaToken.balanceOf(PacaToken.treasury())).to.equal(1000);
+
+            // Simulate another transfer (sell scenario)
+            await expect(PacaToken.connect(user1).transfer(user3.address, 10000))
+                .to.emit(PacaToken, "FeeCharged")
+                .withArgs(PacaToken.treasury(), 1000);
+            expect(await PacaToken.balanceOf(PacaToken.treasury())).to.equal(2000);
+
+            const isZero = await PacaToken.hasRole(DEFAULT_ADMIN_ROLE, zeroAddress);
+            expect(isZero).to.equal(true)
+        });
     });
 });
